641ef10045a6da4ccf2016a69ea49f5160249e68
Melvin Peepers <peepersm@gmail.com>
2019-05-29 21:38:17 -0400

    Finished tweaking css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,11 +3,11 @@ import './Card.css';
 
 const CardContent = () => {
     return (
-        <div className="card-content">
+        <a href="https://reactjs.org"><div className="card-content">
             <h2>Get started with React</h2>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
             <a href="https://www.reactjs.org">reactjs.org</a>
-        </div>
+        </div></a>
     );
 };
 
