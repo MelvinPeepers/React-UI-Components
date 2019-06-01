d63c9f9f93def7ed79355e37e8c26ba2522f32a6
Melvin Peepers <peepersm@gmail.com>
2019-06-01 01:10:50 -0400

    Initial commit for Calculator

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Button.css';
+
+const ButtonContainer = () => {
+    return (
+        <div className="button-container">
+
+        </div>
+
+        <div className="action-button">
+
+        </div>
+
+        <div className="number-button">
+
+        </div>
+
+    );
+};
+
+
+export default ButtonContainer;
