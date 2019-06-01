c16ba9c427d768476f87bed62dfcc13ee3aad7f6
Melvin Peepers <peepersm@gmail.com>
2019-06-01 13:48:13 -0400

    Finished most of the styling for Number and Action Buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -8,22 +8,22 @@ const App = () => {
   return (
     <div className="container">
       <CalculatorDisplay number="0" />
-      <NumberButton text="clear" buttonStyle="num-btn btn"/>
-      <ActionButton action="&#xf7;" buttonStyle="action-btn btn" />
-      <NumberButton text="7" buttonStyle="num-btn btn" />
-      <NumberButton text="8" buttonStyle="num-btn btn" />
-      <NumberButton text="9" buttonStyle="num-btn btn" />
-      <ActionButton action="&#x2715;" buttonStyle="action-btn btn" />
-      <NumberButton text="4" buttonStyle="num-btn btn" />
-      <NumberButton text="5" buttonStyle="num-btn btn" />
-      <NumberButton text="6" buttonStyle="num-btn btn" />
-      <ActionButton action="&#x2212;" buttonStyle="action-btn btn" />
-      <NumberButton text="1" buttonStyle="num-btn btn" />
-      <NumberButton text="2" buttonStyle="num-btn btn" />
-      <NumberButton text="3" buttonStyle="num-btn btn" />
-      <ActionButton action="&#x2b;" buttonStyle="action-btn btn" />
-      <NumberButton text="clear" buttonStyle="num-btn btn" />
-      <ActionButton action="&#x3d;" buttonStyle="action-btn btn" />
+      <NumberButton text="clear" buttonStyle="num-btn btn lg-btn"/>
+      <ActionButton action="&#xf7;" buttonStyle="action-btn btn sm-btn" />
+      <NumberButton text="7" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="8" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="9" buttonStyle="num-btn btn sm-btn" />
+      <ActionButton action="&#xd7;" buttonStyle="action-btn btn sm-btn" />
+      <NumberButton text="4" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="5" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="6" buttonStyle="num-btn btn sm-btn" />
+      <ActionButton action="&#x2212;" buttonStyle="action-btn btn sm-btn" />
+      <NumberButton text="1" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="2" buttonStyle="num-btn btn sm-btn" />
+      <NumberButton text="3" buttonStyle="num-btn btn sm-btn" />
+      <ActionButton action="&#x2b;" buttonStyle="action-btn btn sm-btn" />
+      <NumberButton text="0" buttonStyle="num-btn btn lg-btn sm-btn" />
+      <ActionButton action="&#x3d;" buttonStyle="action-btn btn sm-btn" />
     </div>
   );
 };
